665f623107294c0d0f831056e08dd4e997ad6dfd
Kathy N <kathyvng@gmail.com>
2018-10-31 04:39:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,21 +3,21 @@ class Song
   attr_accessor :name, :artist
 
   @@all = [ ]
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
-    
+
   def self.all
     @@all
   end
 
-  def artist_name 
+  def artist_name
    if self.artist
-     return self.artist.name  
-   else 
-     nil 
+     return self.artist.name
+   else
+     nil
    end 
 
-end 
+end
